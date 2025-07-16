c14f6de1d650b3e5e5cfd2d5abe4ddf0beb3b187
Alan Urmancheev <alan.urman@gmail.com>
2025-07-16 19:55:12 +0400

    Add the main makefile with a stub for school 42's expectations

---
+++ Makefile
@@ -0,0 +1,1 @@
+include school-42.mk
---
+++ school-42.mk
@@ -0,0 +1,30 @@
+NAME := libasm.a
+
+$(NAME):
+	@ echo Not implemented.
+	@ false
+
+.PHONY: all
+all:
+	@ echo Not implemented.
+	@ false
+
+.PHONY: clean
+clean:
+	@ echo Not implemented.
+	@ false
+
+.PHONY: fclean
+fclean:
+	@ echo Not implemented.
+	@ false
+
+.PHONY: re
+re:
+	@ echo Not implemented.
+	@ false
+
+.PHONY: bonus
+bonus:
+	@ echo Not implemented.
+	@ false
